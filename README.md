6ef0a6b865e81d8d24a2614c66b9b3928fcd3e47
Tom <mehta27n@ncssm.edu>
2025-11-28 16:24:54 -0500

    begining of server

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 SOIUGNSOGIN
+* Get /wagon_race - opens the intro page of the website
+* Get /wagon_race/events - set up a SSE stream for push updates. Each update is a JSON-encoded string of gameid and action 
+* Get /wagon_race/game - returns info about the current game using url encoded data
+* * gameid - id of the current game
+* * obstacles - will be added later
+* * players - the list of playerids
+* * positions - the list of player positions as a dictionary of (player id)=(x,y)
+* Get /wagon_race/start - Creates/joins a game using the following url encoded params
+* * gameid - gameid of game to create/join
+* * playerid - player id of the player joining
+* * kart - type of kart of the player wanting to join (light, medium, or heavy)
+* * color - color of the kart of player wanting to join
+* Get /wagon_race/player - returns info about the current player using url encoded data.
+* * position - the players position (x,y)
+* * velocity - the players velocity (xVel,yVel)
+* * acceleration - the players acceleration (xAcc, yAcc)
+* Post /wagon_race/acc - update acceleration with json encoded data
+* * gameid - the game id of the relvant game(might remove later but makes my life easier for now)
+* * playerid - the id of he relevant player
+* * xAcc - the x acceleration of the relevant player
+* * yAcc - the y acceleration of the relevent player
---
+++ mkserver.js
@@ -0,0 +1,126 @@
+const express = require("express")
+const parser = require("body-parser")
+const path = require("path")
+const {v4: uuidv4} = require('uuid')
+
+const cars = require(path.join(__dirname, 'src', 'Car.js'))
+const carphys=require(path.join(__dirname, 'src', 'CarPhysics.ts'))
+
+//should prob implement a class for tracks
+const track=require(path.join(__dirname, 'src', 'track.js'))
+
+
+const games = {}
+const clients = {}
+
+const PORT = 4242
+const app = express();
+app.use("/static", express.static(path.join(__dirname, "public")) )
+app.use(parser.json())
+
+
+app.get("/wagon_race", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+app.get("/wagon_race/start", function(req,res){
+    let gid = req.query.gameid
+    let pid = req.query.playerid
+    let krt = req.query.kart
+    let clr = req.query.color
+    let gm = {
+        track: null,
+        P1: null,
+        P2: null,
+        P3: null,
+        P4: null
+    }
+    let ncr = new ItalianCar(clr,krt)
+    let pl = {
+        gameid: gid,
+        playernumber: null,
+        car: ncr,
+        xacceleration:0,
+        yacceleration:0
+    }
+    if(!(gid in games)){
+        gm[track] = new track()
+        games[gid] = gm
+        
+    }
+    if(games.gid[P1]==null){
+        games.gid[P1] = pid
+        pl[playernumber]=1
+    }
+    else if(games.gid[P2]==null){
+        games.gid[P2] = pid
+        pl[playernumber]=2
+    }
+    else if(games.gid[P3]==null){
+        games.gid[P3] = pid
+        pl[playernumber]=3
+    }
+    else if(games.gid[P4]==null){
+        games.gid[P4] = pid
+        pl[playernumber]=4
+    }
+    else{
+        res.send("game is full")
+        return
+    }
+    clients[pid] = ncr
+
+})
+app.post('/wagon_race/acc', function(req, res){
+    const packet = {
+        status: 'error',
+        message: 'prob an invalid ID or smth'
+    }
+    let gameid = req.body.gameid
+    if( !(gameid in games) ){
+        packet[message] = "invalid gameid"
+        res.send(JSON.stringify(packet))
+        return;
+    }
+    let playerid = req.body.playerid
+    if( !(playerid in clients) ){
+        packet[message] = "invaild playerid"
+        res.send(JSON.stringify(packet))
+        return;
+    }
+    let xa = req.body.xAcc
+    let ya = req.body.yAcc
+    clients.playerid[xacceleration]=xa
+    clients.playerid[xacceleration]=ya
+
+
+})
+
+function sendEvent(gameid, playerId){
+    let packet = JSON.stringify({
+        gameid,
+        playerId,
+        action: 'update'
+    })
+    for( let client in clients ){
+        clients[client].response.write(`data: ${packet}\n\n`)
+    }
+}
+
+app.get("/wagon_race/events", function(req, res){
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders(); 
+
+    let client = {
+        clientID: Date.now(),
+        response: res
+    }
+    clients[client.clientID] = client
+
+    res.on('close', () => {
+        delete clients[client.clientID];
+        res.end();
+    })
+})
