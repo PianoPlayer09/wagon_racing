75c5cd93a7eb5fa2775f635549a9ff059832ddb1
dolphinpod <thomas.fasan2@gmail.com>
2025-12-03 15:35:14 -0500

    feat: move to 0.0.0.0, enabling multi-device connections

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 3000,
+    host: "0.0.0.0",
     open: true,
     proxy: {
-      '/api': 'http://localhost:4242'
+      '/api': 'http://0.0.0.0:4242'
     }
   },
   build: {
